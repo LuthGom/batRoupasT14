d0faef8f45930c3cf7e047258a77a51e5f78ccb6
LuthGom <lucianomendes23@yahoo.com>
2022-05-27 20:17:03 -0300

    Projeto: terminando de ajustar ambiente pré-configurado e instruções no README.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Esse é o ambiente pré-configurado para nossa aula extra de sábado, dia 28/05.
+
+## ⚛ Dê um clone nesse repositório.
+### ⚛ Após o clone, rode:
+    -  npm install.
+    -  cd batRoupasT14.
+    - npm run dev, para rodar a aplicação.
+#### ⚛ Em caso de eventuais erros, pode me chamar antes de 14h para conseguirmos resolver sem tomar tempo da nossa aula extra..
+
+
+##### ⚛ Até lá!
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { msg } from "../mensagem";
 // import Home from "./pages/Home";
 // import Contato from "./pages/Contato";
 // import Sobre from "./pages/Sobre";
@@ -11,12 +13,13 @@ import "./App.css";
 // import Masculino from "./pages/Masculino";
 // import Feminino from "./pages/Feminino";
 // import LGBT from "./pages/LGBTQIA+";
-// import Perfil from "./pages/Perfil";
+
 function App() {
+  const [mensagem, setMensagem] = useState("");
   return (
     <div className="App">
-      <h1>APP</h1>
-      
+      <h1>{mensagem}</h1>
+      <button onClick={() => setMensagem(msg)}>Clique Aqui!</button>
     </div>
   );
 }
