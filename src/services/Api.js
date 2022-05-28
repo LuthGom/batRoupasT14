9bd573b96cf8bfafba5f1ee47eb1d9285a936e10
LuthGom <lucianomendes23@yahoo.com>
2022-05-28 17:43:14 -0300

    Tornando as url's exportáveis para utilizar em todo o projeto

---
+++ src/services/Api.js
@@ -0,0 +1,5 @@
+export const produtosUrl = (categoria) => `https://api-loja-t11.herokuapp.com/produtos${categoria}`;
+
+export const autenticacaoUrl = "https://api-loja-t11.herokuapp.com/usuario";
+
+// "https://api-loja-t11.herokuapp.com/usuarios";
